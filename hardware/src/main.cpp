1711d9c6ea4c11e53b2c02cf2ed2399a45515acb
AzarAI-TOP <3420396703@qq.com>
2024-05-08 22:04:47 +0800

    main test for wifi v1.0

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,15 +1,31 @@
 #include <Arduino.h>
 #include "a_wifi.h"
+#include "a_serial.h"
 
+// the info of WiFi
 const char *ssid = "AzarAI";
 const char *password = "12345678";
 
+// the address of Server
+// const char *server = "ifconfig.me";
+const char *server = "azarai.top";
+const uint16_t port = 80;
+
+WiFiClient client;
+
 void setup() {
     // start Serial
-    Serial.begin(115200);
+    Serial_Init(115200);
     // start WiFi
     WIFI_Init(ssid, password);
+    WIFI_printStatus();
 
+    // test for request
+    WIFI_HTTPRequest(server, port);
+    if (WIFI_HTTPConnected()) {
+        WIFI_HTTPRead();
+        WIFI_HTTPClose();
+    }
 }
 
 void loop() {
