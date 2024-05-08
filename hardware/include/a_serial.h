57ee7ea2b6820abe82502e0ec5ec00256a425322
AzarAI-TOP <3420396703@qq.com>
2024-05-08 22:03:14 +0800

    Initialize the code related to serial

---
+++ hardware/include/a_serial.h
@@ -0,0 +1,8 @@
+#ifndef A_SERIAL_H
+#define A_SERIAL_H
+
+#include <Arduino.h>
+
+void Serial_Init(unsigned long speed);
+
+#endif
---
+++ hardware/src/a_serial.cpp
@@ -0,0 +1,9 @@
+#include "a_serial.h"
+
+
+void Serial_Init(unsigned long speed) {
+    Serial.begin(speed);
+    while (!Serial) {
+        delay(100);
+    }
+}
