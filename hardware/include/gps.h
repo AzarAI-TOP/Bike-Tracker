478bbb63b2bfb51f5ff8445bf6e083a49ff6ac22
AzarAI-TOP <3420396703@qq.com>
2024-05-27 19:36:16 +0800

    modify "gps" into OOP design mode

---
+++ hardware/include/gps.h
@@ -0,0 +1,46 @@
+#ifndef GPS_H
+#define GPS_H
+
+#include <Arduino.h>
+#include <TinyGPSPlus.h>
+
+struct GPS_Data
+{
+    // date data
+    uint16_t year;
+    uint8_t month;
+    uint8_t day;
+    // time data
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+    // location data
+    double longitude;
+    double latitude;
+    // speed data
+    double speed;
+
+    //
+};
+
+
+class GPS {
+
+private:
+    TinyGPSPlus gps_parser;
+    HardwareSerial gps_serial = Serial1;
+
+public:
+    GPS();
+    void GPS_Init(uint8_t rxPin, uint8_t txPin, unsigned long baud);
+    ~GPS(void);
+
+    bool GPS_Available(void);
+    int GPS_Read(void);
+    void GPS_Parse(void);
+    bool GPS_IsChangedLocation(void);
+    GPS_Data GPS_GetData(void);
+
+};
+
+#endif
---
+++ hardware/src/gps.cpp
@@ -0,0 +1,47 @@
+#include "gps.h"
+
+GPS::GPS() {}
+
+void GPS::GPS_Init(uint8_t rxPin, uint8_t txPin, unsigned long baud) {
+    // begin the serial connected to GPS module
+    gps_serial.begin(baud, SERIAL_8N1, rxPin, txPin);
+}
+
+GPS::~GPS(void) {
+    gps_serial.end();
+}
+
+bool GPS::GPS_Available(void) {
+    return gps_serial.available() > 0;
+}
+
+int GPS::GPS_Read(void) {
+    return gps_serial.read();
+}
+
+void GPS::GPS_Parse(void) {
+    // read until a valid sentence
+    // gps_serial.flush();
+    while(GPS_Available() && !gps_parser.encode(GPS_Read())) ;
+
+}
+
+bool GPS::GPS_IsChangedLocation(void) {
+    return gps_parser.location.isUpdated();
+}
+
+GPS_Data GPS::GPS_GetData(void) {
+    // parse the gps info into structure and return it
+    GPS_Data data = {
+                .year = gps_parser.date.year(),
+                .month = gps_parser.date.month(),
+                .day = gps_parser.date.day(),
+                .hour = gps_parser.time.hour(),
+                .minute = gps_parser.time.minute(),
+                .second = gps_parser.time.second(),
+                .longitude = gps_parser.location.lng(),
+                .latitude = gps_parser.location.lat(),
+                .speed = gps_parser.speed.mps()
+            };
+            return data;
+}
