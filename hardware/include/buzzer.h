1742fbdf5536b178a6303218c481efbca7f555eb
AzarAI-TOP <3420396703@qq.com>
2024-05-28 12:40:52 +0800

    add numberic check

--- hardware/include/buzzer.h
+++ hardware/include/buzzer.h
@@ -3,14 +3,17 @@
 
 #include <Arduino.h>
 
+#define BUZZER_DEFAULT_FREQ 3000
+#define BUZZER_DEFAULT_TIME 1000
+
 class Buzzer
 {
 private:
     uint8_t buzzer_pin;
 public:
     Buzzer();
-    void Buzzer_Init(uint8_t pin);
     ~Buzzer();
+    void Buzzer_Init(uint8_t pin);
 
     void beep(int frequency, int milleseconds);
     void on(int frequency);
--- hardware/src/buzzer.cpp
+++ hardware/src/buzzer.cpp
@@ -1,24 +1,23 @@
 #include "buzzer.h"
 
 Buzzer::Buzzer() {}
+Buzzer::~Buzzer() {
+    noTone(buzzer_pin);
+}
 
 void Buzzer::Buzzer_Init(uint8_t pin) {
     buzzer_pin = pin;
     pinMode(buzzer_pin, OUTPUT);
 }
 
-Buzzer::~Buzzer() {
-    noTone(buzzer_pin);
-}
-
-void Buzzer_Init(void) {
-}
-
 void Buzzer::beep(int frequency, int milleseconds) {
+    if (frequency <= 0) frequency = BUZZER_DEFAULT_FREQ;
+    if (milleseconds <= 0) milleseconds = BUZZER_DEFAULT_TIME;
     tone(buzzer_pin, frequency, milleseconds);
 }
 
 void Buzzer::on(int frequency) {
+    if (frequency <= 0) frequency = BUZZER_DEFAULT_FREQ;
     tone(buzzer_pin, frequency);
 }
 
